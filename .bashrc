fc3d601aa4533315b8985b483762b23974c4c135
Ethan Nagola <enagola@ucsd.edu>
2021-01-28 10:53:27 -0800

    finish up to slide 34

---
+++ .bashrc
@@ -0,0 +1,3 @@
+npm set prefix ~/.npm
+PATH="$HOME/.npm/bin:$PATH"
+PATH="./node_modules/.bin:$PATH"
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,24 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index', {
+    "projects": [
+      { "name": "Waiting in Line",
+      "image": "lorempixel.people.1.jpeg",
+      "id": "project1"
+    },
+    { "name": "Needfinding",
+    "image": "lorempixel.people.1.jpeg",
+    "id": "project2"
+    },
+    { "name": "Doing Work",
+    "image": "lorempixel.people.1.jpeg",
+    "id": "project3"
+    },
+    { "name": "Finshing work",
+    "image": "lorempixel.people.1.jpeg",
+    "id": "project4"
+    }
+  ]
+  });
 };
